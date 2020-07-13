06a4b5ba8e4be535ba3b56f31a62f201bfdcddd9
adamovski <45078250+Adamovski@users.noreply.github.com>
2020-07-13 18:42:07 +0200

    Updates

---
+++ precache-manifest.9626af50b13cba9c0d3f45a433fbbd77.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6272df9761d557ff05a67b2ea79877",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "34ba60f0166c2f8c3e6b",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "069e9f600b5cca44683d",
+    "url": "/portfolio/static/js/2.8aaa3283.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.8aaa3283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34ba60f0166c2f8c3e6b",
+    "url": "/portfolio/static/js/main.511c5771.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "b75d4ea8b35eaf23f8a1cd681a646612",
+    "url": "/portfolio/static/media/Screenshot 2020-06-09 at 19.00.09.b75d4ea8.png"
+  },
+  {
+    "revision": "5c2df16b68ba9eb3ac781cc2acdffc7b",
+    "url": "/portfolio/static/media/Screenshot 2020-06-09 at 20.15.33.5c2df16b.png"
+  },
+  {
+    "revision": "fa7a480fca0e467e7d4ee2700289898a",
+    "url": "/portfolio/static/media/Screenshot 2020-06-10 at 16.20.00.fa7a480f.png"
+  },
+  {
+    "revision": "0e1a44a28004546291cb516ace500f79",
+    "url": "/portfolio/static/media/colour-1885352_1920.0e1a44a2.jpg"
+  }
+]);
